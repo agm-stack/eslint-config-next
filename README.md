691b64f5e9baf7593ed526424673d5a03a0b2cc1
Adrian Manduc <contact@adrianmanduc.com>
2024-12-01 18:03:03 +0200

    Upgrade to ESLint 9 (flat config) and add tailwind rules

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
 # @agm-stack/eslint-config-next
 
-A custom ESLint configuration for Next.js 15+ projects using TypeScript. This package provides an opinionated ESLint setup tailored for Next.js and TypeScript development, including stylistic rules, import sorting, and React-specific linting.
+A tailored ESLint configuration for Next.js 15+ projects leveraging TypeScript, requiring ESLint version 9 or above. This package offers a comprehensive ESLint setup optimized for Next.js and TypeScript development, featuring stylistic rules, import sorting, and React-specific linting guidelines.
 
 ---
 
 ## Features
 
-- Next.js-specific rules via [`eslint-config-next`](https://www.npmjs.com/package/eslint-config-next).
-- TypeScript support with [`@typescript-eslint`](https://github.com/typescript-eslint/typescript-eslint).
-- Stylistic rules with [`@stylistic/eslint-plugin`](https://www.npmjs.com/package/@stylistic/eslint-plugin).
-- Destructuring key sorting with [`eslint-plugin-sort-destructure-keys`](https://www.npmjs.com/package/eslint-plugin-sort-destructure-keys).
-- Automatically enforces best practices for React, imports, and TypeScript codebases.
+- Incorporates Next.js-specific rules through [`eslint-config-next`](https://www.npmjs.com/package/eslint-config-next).
+- Provides TypeScript support using [`@typescript-eslint`](https://github.com/typescript-eslint/typescript-eslint).
+- Enforces stylistic standards with [`@stylistic/eslint-plugin`](https://www.npmjs.com/package/@stylistic/eslint-plugin).
+- Ensures consistent destructuring key order via [`eslint-plugin-sort-destructure-keys`](https://www.npmjs.com/package/eslint-plugin-sort-destructure-keys).
+- Automatically promotes best practices for React, imports, and TypeScript codebases.
 
 ---
 
 ## Installation
 
-To install the package along with its required dependencies, run:
+Install the package along with its necessary dependencies by executing:
 
 ```bash
 npm install --save-dev @agm-stack/eslint-config-next
@@ -25,18 +25,22 @@ npm install --save-dev @agm-stack/eslint-config-next
 ## Usage
 
 ```javascript
-module.exports = {
-  extends: ["@agm-stack/eslint-config-next"]
-};
+import eslintConfigNext from '@agm-stack/eslint-config-next/index.mjs';
+
+
+const config = [
+  ...eslintConfigNext
+];
+
+export default config;
 ```
 
 ## Peer Dependencies
 
 This package relies on the following peer dependencies, which will not be installed automatically:
 
-eslint (v8)
-eslint-config-next (v15+)
-typescript (v4+)
+eslint (v9+)
+typescript (v5+)
 To install the required peer dependencies, run:
 
 ```bash
--- index.js
+++ index.js
@@ -1,267 +1,328 @@
-module.exports = {
-  extends: [
-    "next/core-web-vitals",
-    "next/typescript"
-  ],
-  globals: {
-    React: true,
-    JSX: true
-  },
-  plugins: ["@stylistic", "sort-destructure-keys"],
-  rules: {
-    // TypeScript Rules
-    "@typescript-eslint/no-explicit-any": ["warn"],
-    "@typescript-eslint/consistent-type-definitions": ["warn", "type"],
-    "@typescript-eslint/consistent-type-imports": "warn",
-    "@typescript-eslint/no-shadow": "error",
-    "@typescript-eslint/no-unused-vars": ["warn"],
+const { FlatCompat } = require("@eslint/eslintrc");
+const tseslint = require("typescript-eslint");
+const eslint = require("@eslint/js");
+const hooks = require("eslint-plugin-react-hooks");
+const stylistic = require("@stylistic/eslint-plugin");
+const react = require("eslint-plugin-react");
+const parser = require("@typescript-eslint/parser");
+const importPlugin = require("eslint-plugin-import");
+const sortKeys = require("eslint-plugin-sort-destructure-keys");
+const readableTailwind = require('eslint-plugin-readable-tailwind');
+const tailwind = require('eslint-plugin-tailwindcss');
 
-    // Stylistic Rules
-    "@stylistic/object-curly-spacing": ["warn", "always"],
-    "@stylistic/type-annotation-spacing": "warn",
-    "@stylistic/member-delimiter-style": [
-      "warn",
-      {
-        multiline: {
-          delimiter: "comma",
-          requireLast: false,
-        },
-        singleline: {
-          delimiter: "comma",
-          requireLast: false,
+const compat = new FlatCompat({
+  baseDirectory: __dirname
+});
+
+const config = [
+  ...tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.strict,
+    tseslint.configs.stylistic
+  ),
+  react.configs.flat.recommended,
+  ...compat.extends("eslint-config-next"),
+  ...tailwind.configs['flat/recommended'],
+  {
+    name: 'ESLint Config - Next.JS',
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
         },
       },
-    ],
-    "@stylistic/object-curly-newline": [
-      "error",
-      {
-        ExportDeclaration: {
-          consistent: true,
-          minProperties: 5,
-          multiline: true,
+    },
+    plugins: {
+      '@stylistic': stylistic,
+      'react-hooks': hooks,
+      'import': importPlugin,
+      'sort-destructure-keys': sortKeys,
+      'readable-tailwind': readableTailwind
+    },
+    rules: {
+      ...hooks.configs.recommended.rules,
+
+      // Core
+      "consistent-return": "off",
+      "default-param-last": "off",
+      "prefer-promise-reject-errors": ["warn"],
+      "sort-keys": "off",
+      "no-unused-vars": "off",
+      "sort-imports": [
+        "error",
+        {
+          ignoreDeclarationSort: true,
         },
-        ImportDeclaration: {
-          consistent: true,
-          minProperties: 5,
-          multiline: true,
+      ],
+      "no-debugger": "warn",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-shadow": "off",
+      "no-undef": ["error"],
+      "no-useless-assignment": "off",
+      "no-use-before-define": "off",
+
+      // Tailwind Rules
+      'readable-tailwind/multiline': ['warn', {
+        classesPerLine: 14, group: 'newLine', preferSingleLine: true, lineBreakStyle: 'windows'
+      }],
+      'readable-tailwind/no-unnecessary-whitespace': ['warn', { allowMultiline: true, }],
+
+      // TypeScript Rules
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-explicit-any": ["warn"],
+      "@typescript-eslint/consistent-type-definitions": ["warn", "type"],
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/no-shadow": "warn",
+      "@typescript-eslint/no-unused-vars": ["warn", {
+        "args": "all",
+        "argsIgnorePattern": "^_",
+        "caughtErrors": "all",
+        "caughtErrorsIgnorePattern": "^_",
+        "destructuredArrayIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "ignoreRestSiblings": true
+      }],
+
+      // Stylistic Rules
+      "sort-destructure-keys/sort-destructure-keys": "warn",
+      "@stylistic/padded-blocks": "off",
+      "@stylistic/quote-props": ["error", "as-needed"],
+      "@stylistic/no-multi-spaces": [
+        "error",
+        {
+          exceptions: {},
         },
-        ObjectExpression: {
-          consistent: true,
-          minProperties: 5,
-          multiline: true,
-        },
-        ObjectPattern: {
-          consistent: true,
-          minProperties: 5,
-          multiline: true,
-        },
-      },
-    ],
-    "object-curly-spacing": ["error", "always"],
-    "object-property-newline": [
-      "error",
-      {
-        allowAllPropertiesOnSameLine: true,
-        allowMultiplePropertiesPerLine: false,
-      },
-    ],
-    "array-bracket-spacing": ["error", "never"],
-    "comma-spacing": [
-      "error",
-      {
-        after: true,
-        before: false,
-      },
-    ],
-    "key-spacing": ["error"],
-    "keyword-spacing": [
-      "error",
-      {
-        after: true,
-        before: true,
-      },
-    ],
-    "space-before-blocks": ["warn", "always"],
-    "space-before-function-paren": [
-      "error",
-      {
-        anonymous: "never",
-        asyncArrow: "always",
-        named: "never",
-      },
-    ],
-    "space-in-parens": ["error", "never"],
-    "space-infix-ops": "warn",
-    "spaced-comment": [
-      "warn",
-      "always",
-      {
-        line: {
-          exceptions: ["-", "+"],
-          markers: ["=", "!"]
-        },
-        block: {
-          balanced: true,
-          markers: ["!"],
-          exceptions: ["*"]
+      ],
+      "@stylistic/no-multiple-empty-lines": [
+        "error",
+        {
+          "max": 2,
+          "maxEOF": 0,
+          "maxBOF": 1
         }
-      }
-    ],
+      ],
+      "@stylistic/no-trailing-spaces": "error",
+      "@stylistic/max-len": [
+        "warn",
+        {
+          code: 175,
+        },
+      ],
+      "@stylistic/indent": [
+        "warn",
+        2,
+        {
+          SwitchCase: 1,
+          ignoredNodes: ["TemplateLiteral"],
+        },
+      ],
+      "@stylistic/newline-per-chained-call": ["warn", { ignoreChainWithDepth: 3 }],
+      "@stylistic/semi": ["warn", "always"],
+      "@stylistic/object-curly-spacing": ["warn", "always"],
+      "@stylistic/type-annotation-spacing": "warn",
+      "@stylistic/member-delimiter-style": [
+        "warn",
+        {
+          multiline: {
+            delimiter: "comma",
+            requireLast: false,
+          },
+          singleline: {
+            delimiter: "comma",
+            requireLast: false,
+          },
+        },
+      ],
+      "@stylistic/object-curly-newline": [
+        "error",
+        {
+          ExportDeclaration: {
+            consistent: true,
+            minProperties: 5,
+            multiline: true,
+          },
+          ImportDeclaration: {
+            consistent: true,
+            minProperties: 5,
+            multiline: true,
+          },
+          ObjectExpression: {
+            consistent: true,
+            minProperties: 5,
+            multiline: true,
+          },
+          ObjectPattern: {
+            consistent: true,
+            minProperties: 5,
+            multiline: true,
+          },
+        },
+      ],
+      "@stylistic/object-curly-spacing": ["error", "always"],
+      "@stylistic/object-property-newline": [
+        "error",
+        {
+          allowAllPropertiesOnSameLine: true,
+          allowMultiplePropertiesPerLine: false,
+        },
+      ],
+      "@stylistic/array-bracket-spacing": ["error", "never"],
+      "@stylistic/comma-spacing": [
+        "error",
+        {
+          after: true,
+          before: false,
+        },
+      ],
+      "@stylistic/key-spacing": ["error"],
+      "@stylistic/keyword-spacing": [
+        "error",
+        {
+          after: true,
+          before: true,
+        },
+      ],
+      "@stylistic/space-before-blocks": ["warn", "always"],
+      "@stylistic/space-before-function-paren": [
+        "error",
+        {
+          anonymous: "never",
+          asyncArrow: "always",
+          named: "never",
+        },
+      ],
+      "@stylistic/space-in-parens": ["error", "never"],
+      "@stylistic/space-infix-ops": "warn",
+      "@stylistic/spaced-comment": [
+        "warn",
+        "always",
+        {
+          line: {
+            exceptions: ["-", "+"],
+            markers: ["=", "!"]
+          },
+          block: {
+            balanced: true,
+            markers: ["!"],
+            exceptions: ["*"]
+          }
+        }
+      ],
 
     // React Rules
-    "react-hooks/exhaustive-deps": "warn",
-    "react-hooks/rules-of-hooks": "error",
-    "react/destructuring-assignment": "warn",
-    "react/function-component-definition": [
-      "error",
-      {
-        namedComponents: "function-declaration",
-      },
-    ],
-    "react/jsx-closing-bracket-location": [1, "tag-aligned"],
-    "react/jsx-curly-brace-presence": "error",
-    "react/jsx-first-prop-new-line": [2, "multiline"],
-    "react/jsx-indent-props": [2, 2],
-    "react/jsx-max-props-per-line": [
-      1,
-      {
-        maximum: {
-          multi: 1,
-          single: 5,
-        },
-      },
-    ],
-    "react/jsx-one-expression-per-line": [
-      1,
-      {
-        allow: "single-child",
-      },
-    ],
-    "react/jsx-props-no-multi-spaces": ["error"],
-    "react/jsx-props-no-spreading": "off",
-    "react/jsx-tag-spacing": [
-      1,
-      {
-        beforeSelfClosing: "always",
-      },
-    ],
-    "react/jsx-no-useless-fragment": "warn",
-    "react/jsx-curly-newline": [
-      "warn",
-      {
-        multiline: "consistent",
-        singleline: "consistent"
-      }
-    ],
-    "react/jsx-sort-props": [
-      "warn",
-      {
-        callbacksLast: true,
-        shorthandFirst: true,
-        noSortAlphabetically: false,
-        reservedFirst: true
-      }
-    ],
-    "react/jsx-wrap-multilines": [
-      "warn",
-      { declaration: "parens-new-line", assignment: "parens-new-line", return: "parens-new-line" }
-    ],
-    "react/prop-types": "off",
-    "react/react-in-jsx-scope": "off",
-    "react/require-default-props": "off",
-    "react/jsx-uses-react": "off",
+     "react-hooks/exhaustive-deps": "warn",
+     "react-hooks/rules-of-hooks": "error",
+     "react/destructuring-assignment": "warn",
+     "react/function-component-definition": [
+       "warn",
+       {
+         namedComponents: "function-declaration",
+       },
+     ],
+     "react/jsx-closing-bracket-location": [1, "tag-aligned"],
+     "react/jsx-curly-brace-presence": "error",
+     "react/jsx-first-prop-new-line": [2, "multiline"],
+     "react/jsx-indent-props": [2, 2],
+     "react/jsx-max-props-per-line": [
+       1,
+       {
+         maximum: {
+           multi: 1,
+           single: 5,
+         },
+       },
+     ],
+     "react/jsx-one-expression-per-line": [
+       1,
+       {
+         allow: "single-child",
+       },
+     ],
+     "react/jsx-props-no-multi-spaces": ["error"],
+     "react/jsx-props-no-spreading": "off",
+     "react/jsx-tag-spacing": [
+       1,
+       {
+         beforeSelfClosing: "always",
+       },
+     ],
+     "react/jsx-no-useless-fragment": "warn",
+     "react/jsx-curly-newline": [
+       "warn",
+       {
+         multiline: "consistent",
+         singleline: "consistent"
+       }
+     ],
+     "react/jsx-sort-props": [
+       "warn",
+       {
+         callbacksLast: true,
+         shorthandFirst: true,
+         noSortAlphabetically: false,
+         reservedFirst: true
+       }
+     ],
+     "react/jsx-wrap-multilines": [
+       "warn",
+       { declaration: "parens-new-line", assignment: "parens-new-line", return: "parens-new-line" }
+     ],
+     "react/prop-types": "off",
+     "react/react-in-jsx-scope": "off",
+     "react/require-default-props": "off",
+     "react/jsx-uses-react": "off",
 
-    // Import Rules
-    "import/no-anonymous-default-export": "off",
-    "import/no-unresolved": ["error"],
-    "import/order": [
-      "warn",
-      {
-        alphabetize: {
-          caseInsensitive: true,
-          order: "asc",
+      // Import Rules
+      "import/no-anonymous-default-export": "off",
+      "import/no-unresolved": ["error"],
+      "import/no-anonymous-default-export": "off",
+      "import/no-unresolved": ["error"],
+      "import/order": [
+        "warn",
+        {
+          alphabetize: {
+            caseInsensitive: true,
+            order: "asc",
+          },
+          groups: [
+            "builtin",
+            "external",
+            "internal",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type",
+          ],
+          "newlines-between": "always-and-inside-groups",
+          pathGroups: [
+            {
+              group: "type",
+              pattern: "types",
+              position: "after",
+            },
+            {
+              group: "object",
+              pattern: "{.,..}/**/*.scss",
+              position: "after",
+            },
+          ],
         },
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-          "object",
-          "type",
-        ],
-        "newlines-between": "always-and-inside-groups",
-        pathGroups: [
-          {
-            group: "type",
-            pattern: "types",
-            position: "after",
-          },
-          {
-            group: "object",
-            pattern: "{.,..}/**/*.scss",
-            position: "after",
-          },
-        ],
-      },
-    ],
-    "import/prefer-default-export": "off",
+      ],
+      "import/prefer-default-export": "off",
+    },
+  }
+];
 
-    // Best Practices
-    "consistent-return": "off",
-    "default-param-last": "off",
-    "newline-per-chained-call": ["warn", { ignoreChainWithDepth: 3 }],
-    "padded-blocks": "off",
-    "prefer-promise-reject-errors": "off",
-    "quote-props": ["error", "as-needed"],
-    semi: ["error", "always"],
-    "sort-destructure-keys/sort-destructure-keys": "warn",
-    "sort-imports": [
-      "error",
-      {
-        ignoreDeclarationSort: true,
-      },
-    ],
-    "no-debugger": "warn",
-    "no-console": ["warn", { allow: ["warn", "error"] }],
-    "no-multi-spaces": [
-      "error",
-      {
-        exceptions: {},
-      },
-    ],
-    "no-multiple-empty-lines": [
-      "error",
-      {
-        "max": 2,
-        "maxEOF": 0,
-        "maxBOF": 1
-      }
-    ],
-    "no-trailing-spaces": "error",
-    "no-shadow": "off",
-    "no-undef": ["error"],
-    "no-unused-vars": "off",
-    "no-use-before-define": "off",
-    "max-len": [
-      "warn",
-      {
-        code: 175,
-      },
-    ],
-    indent: [
-      "warn",
-      2,
-      {
-        SwitchCase: 1,
-        ignoredNodes: ["TemplateLiteral"],
-      },
-    ],
-  },
-  settings: {
-    react: {
-      version: "detect"
-    }
-  },
-};
+module.exports = {
+  flat: config
+}
